8e2bb2ec2315207c933c5496a1750f0057042d55
kegus <kegus@mail.ru>
2019-06-07 17:15:38 +0600

    добавил первый метод.

--- src/Lesson5/Program5.java
+++ src/Lesson5/Program5.java
@@ -1,7 +1,20 @@
 package Lesson5;
 
 public class Program5 {
+    static final int size = 10000000;
+    static final int h = size / 2;
+
     public static void main(String[] args) {
+        float[] arr = new float[size];
+        long a;
+        for (int i = 0; i < size; i++) arr[i] = 1;
+
+        // 1-й метод
+        a = System.currentTimeMillis();
+        for (int i = 0; i < size; i++)
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+
+        System.out.println(System.currentTimeMillis() - a);
         System.out.println("Ok");
     }
 }
