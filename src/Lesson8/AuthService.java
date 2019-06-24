fec380caf503950786b7e3dc73506d86fbac9a59
kegus <kegus@mail.ru>
2019-06-25 01:06:00 +0600

    initial commit for Lesson8.

---
+++ src/Lesson8/AuthService.java
@@ -0,0 +1,17 @@
+package Lesson8;
+
+import java.util.List;
+
+public class AuthService {
+    private List<String> nicksLst;
+
+    public String checkNick(String nick) {
+        return (nicksLst.contains(nick)?null:nick);
+    }
+    public void start(List<String> nicksLst){
+        this.nicksLst = nicksLst;
+    }
+    public void stop(){
+
+    }
+}
